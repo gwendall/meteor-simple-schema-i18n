e8b0258821e7d1dd12429f86b934cc1e185d6f11
Gwen <gwendall.esnault@gmail.com>
2015-04-17 17:21:51 +0200

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+Meteor Autoform i18n
+====================
+
+Intenationalization for Autoform
+
+Installation  
+------------
+
+``` sh
+meteor add gwendall:autoform-i18n
+```
+
+Contribute
+----------
+
+To contribute to the project, please fork the repo and submit a pull request after adding or modifying any language file (in the /i18n folder).
---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: "gwendall:autoform-i18n",
+  summary: "Internationalization for Autoform",
+  version: "0.1.0",
+  git: "https://github.com/gwendall/meteor-autoform-i18n.git"
+});
+
+Package.onUse(function (api, where) {
+
+  api.use([
+    "aldeed:simple-schema@1.3.2",
+    "tap:i18n@1.4.1",
+    "templating@1.1.1",
+    "underscore@1.0.3"
+  ]);
+
+  api.add_files([
+    "client/lib.js"
+  ], "client");
+
+  api.addFiles("package-tap.i18n");
+  api.addFiles([
+    "i18n/en.i18n.json",
+    "i18n/fr.i18n.json",
+  ]);
+
+});
