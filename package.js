9d890dbfa00d549bcb293a98a3ec9ffc1ac44edf
Gwen <gwendall.esnault@gmail.com>
2015-04-18 14:13:03 +0200

    version update

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "gwendall:autoform-i18n",
     summary: "Internationalization for Autoform",
-    version: "0.1.2",
+    version: "0.1.3",
     git: "https://github.com/gwendall/meteor-autoform-i18n.git"
 });
 
