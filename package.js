b6a5d031814351dde7b06327d0e3d9bec8e57216
Gwendall Esnault <gwendall.esnault@gmail.com>
2017-08-07 14:41:58 +0200

    fix japanese tag

--- package.js
+++ package.js
@@ -33,7 +33,7 @@ Package.onUse(function(api, where) {
     'i18n/hu.i18n.json',
     'i18n/id.i18n.json',
     'i18n/it.i18n.json',
-    'i18n/jp.i18n.json',
+    'i18n/ja.i18n.json',
     'i18n/nb.i18n.json',
     'i18n/nl.i18n.json',
     'i18n/pl.i18n.json',
